6d158ff0aaafe71a72161a1bc5e05d917685e3bf
Sebastián López Miranda <sebas.lopezm24@gmail.com>
2018-10-11 19:54:47 -0500

    Lesson 2 finished

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
+/*
+ *This file will have some basic exercises from Udacity's Intro to JS course
+ */
+
+
+ // Hello World test
 var msg = 'Hello World';
 
 console.log(msg);
 
+// Celsius to Fahrenheit
+var celsius = 12;
+var fahrenheit = celsius*1.8+32 /* convert celsius to fahrenheit here */
 
+console.log(fahrenheit/* print out result here */);
+
+
+// String indexing 
+
+var quote = "Stay awhile and listen!";
+console.log(quote[6]);
+
+//Escaping Strings
+
+console.log("The file located at \"C:\\\\Desktop\\My Documents\\Roster\\names.txt\" contains the names on the roster.")
+console.log("This is a \n new line and \t tab test");
+
+/*
+ * Programming Quiz: Yosa Buson (2-6)
+ */
+
+var haiku = 'Blowing from the west' + '\nFallen leaves gather' + '\nIn the east.'
+console.log(haiku);
