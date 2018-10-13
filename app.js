a68b338d76786c7ad5ce71b4acb506baa9db00f0
Sebastián López Miranda <sebas.lopezm24@gmail.com>
2018-10-13 18:34:15 -0500

    Couse finished

--- app.js
+++ app.js
@@ -154,3 +154,37 @@ for (var row = 0; row < donutBox.length; row++) {
     console.log(donutBox[row][column]);
   }
 }   
+
+
+/*
+*Objects
+*/
+
+/*
+ * Programming Quiz: Umbrella (7-1)
+ */
+
+var umbrella = {
+  color: "pink",
+  isOpen: true,
+  open: function() {
+      if (umbrella.isOpen === true) {
+          return "The umbrella is already opened!";
+      } else {
+          umbrella.isOpen = true;
+          return "Julia opens the umbrella!";
+      }
+  },
+  
+  close: function() {
+      if (umbrella.isOpen === false) {
+          return "The umbrella is already closed!";
+      } else {
+          umbrella.isOpen = false;
+          return "Julia closes the umbrella!";
+      }
+  },
+};
+
+console.log(umbrella.open());
+console.log(umbrella.close());
